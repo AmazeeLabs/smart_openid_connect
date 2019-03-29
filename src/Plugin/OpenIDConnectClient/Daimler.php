1778c6a62c710cd464f1f067656b32f8014ebbc9
= <blaize.kaye@gmail.com>
2019-03-29 16:06:33 +1300

    Adds basic user info functionality

--- src/Plugin/OpenIDConnectClient/Daimler.php
+++ src/Plugin/OpenIDConnectClient/Daimler.php
@@ -47,8 +47,8 @@ class Daimler extends OpenIDConnectClientBase {
    * {@inheritdoc}
    */
   public function authorize($scope = 'openid profile') {
-    // Use Facebook specific authorisations.
-    return parent::authorize('');
+    // Use Daimler specific authorisations.
+    return parent::authorize('openid');
   }
 
   /**
@@ -62,4 +62,42 @@ class Daimler extends OpenIDConnectClientBase {
     ];
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function retrieveUserInfo($access_token) {
+    $request_options = [
+      'headers' => [
+        'Authorization' => 'Bearer ' . $access_token,
+        'Accept' => 'application/json',
+      ],
+    ];
+
+    $endpoints = $this->getEndpoints();
+
+    $client = $this->httpClient;
+    try {
+      $response = $client->get($endpoints['userinfo'], $request_options);
+
+      $response_data = json_decode((string) $response->getBody(), TRUE);
+
+      if(!array_key_exists('sub', $response_data)) {
+        throw new \Exception("No subject returned from call");
+      }
+
+      return [
+          'sub' => $response_data['sub'],
+          'email' => $response_data['sub'] . "@daimler.com" //TODO: this should come from the userinfo endpoint
+      ];
+    }
+    catch (Exception $e) {
+      $variables = [
+        '@message' => 'Could not retrieve user profile information',
+        '@error_message' => $e->getMessage(),
+      ];
+      $this->loggerFactory->get('openid_connect_' . $this->pluginId)
+        ->error('@message. Details: @error_message', $variables);
+      return FALSE;
+    }
+  }
 }
